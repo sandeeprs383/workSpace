0ba2d6bf16e7405e919e5b315ade578bfa282e21
sandeep <sandeeprs383@gmail.com>
2022-10-26 18:27:00 +0100

    all assignments

---
+++ Assign_2.java
@@ -0,0 +1,28 @@
+
+public class Assign_2 {
+	public static void main(String[] args) {
+		
+		int n=5;
+				
+		int k=1;
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			
+				System.out.print(k);
+		
+		}
+		System.out.println();
+		k++;
+	}
+		
+		
+		
+		
+		
+	}
+	
+		
+	
+	
+
+}
---
+++ Assign_4.java
@@ -0,0 +1,16 @@
+
+public class Assign_4 {
+	public static void main(String[] args) {
+		int n=27;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(j==0&&i>=n/2||i==n-1||i-j>=n/2||j==n-1&&i>=n/2||i+j>=n-1+(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
